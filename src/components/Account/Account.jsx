fcbf28d6ef7df6c646a8c4c9f245a519e4cf5b9c
Aaditya Chaudhary <caaditya495@gmail.com>
2024-08-04 20:56:58 +0545

    modify signup form

--- src/components/Account/Account.jsx
+++ src/components/Account/Account.jsx
@@ -5,20 +5,25 @@ function Account() {
   return (
       <>
             <div className='h-screen flex items-center justify-center'>
-                  <div className='shadow-2xl'>
+                  <div className='shadow-2xl rounded-2xl'>
                         <form action="" className='flex flex-col items-center p-10 gap-6'>
-                              <div className='flex flex-col gap-6'>
-                                    <SignupInput type="text" placeholder= "First Name" />
+                              <div className='flex flex-col gap-2'>
+                                    
+                                    <SignupInput label="Name" type="text" placeholder= "Name" name="name" />
 
-                                    <SignupInput type="text" placeholder= "Last Name" />
+                                    <SignupInput label="Username" type="text" placeholder= "Username" name="username" />
 
-                                    <SignupInput type="email" placeholder= "Email" />
+                                    <SignupInput label="Email" type="email" placeholder= "Email" name="email" />
 
-                                    <SignupInput type="password" placeholder= "Password" />
+                                    <SignupInput label="Phone number" type="text" placeholder= "Phone number" name="phoneNumber" />
+
+                                    <SignupInput label="Password" type="password" placeholder= "Password" name="password" />
+
+                                    <SignupInput label="Confirm Password" type="password" placeholder= "Confirm password" name="confirmPasswrod" /> 
                               </div>
 
                               <div className='flex justify-center'>
-                                    <button type="submit" className='bg-sky-500 text-white px-8 py-1 font-semibold rounded-lg'>Sign up</button>
+                                    <button type="submit" className='bg-sky-500 text-white px-20 py-2 font-semibold rounded-lg'>Sign up</button>
                               </div>
 
                         </form>
--- src/components/Account/SignupInput.jsx
+++ src/components/Account/SignupInput.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 
 
-function SignupInput({type, placeholder}) {
+function SignupInput({type, placeholder, label, name}) {
   return (
     <>
-      <div>
-        <input type={type} placeholder={placeholder} className='border border-black rounded-lg px-10 py-1 pl-2 pr-32 font-normal' />
+      <div className='flex flex-col'>
+        <label htmlFor={name}> {label}</label>
+        <input type={type} placeholder={placeholder} id={name} className='border border-black rounded-lg px-10 py-1 pl-2 pr-32 font-normal'/>
       </div>
     </>
   )
