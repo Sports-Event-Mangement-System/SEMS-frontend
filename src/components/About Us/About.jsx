d91600ce72d13dee45dc6e6c2a308b664b1a7599
codewithsubash <155702363+codewithsubash@users.noreply.github.com>
2024-08-07 20:14:46 +0545

    Home and About page design

--- src/components/About Us/About.jsx
+++ src/components/About Us/About.jsx
@@ -3,8 +3,39 @@ import React from 'react'
 function About() {
   return (
     <>
-      <div className='h-screen'>
-        About
+      <div className='h-screen flex justify-center'>
+        <div className='w-[90%] h-[84vh] p-10 bg-white drop-shadow-[0_7px_6px_rgba(0,0,0,0.25)] rounded-2xl flex flex-col item-center'>
+          <h1 className='text-3xl font-bold text-center mt-4'>About <span className='text-orange-600'>Us</span></h1>
+          <div className='flex flex-row justify-center gap-16 my-8'>
+
+            <div className='w-[50vh] h-[52vh]'>
+
+              <img src="images/about1.jpg" alt="" className='w-[173px] h-[189px] rounded-[15px] drop-shadow-[0_7px_6px_rgba(0,0,0,0.25)]' />
+
+
+              <img src="images/about2.jpg" alt="" className='w-[162px] h-[162px] rounded-full object-cover relative bottom-[169px] left-[176px] drop-shadow-[0_7px_6px_rgba(0,0,0,0.25)]' />
+
+
+              <img src="images/about3.jpg" alt="" className='w-[168px] h-[144px] object-cover relative bottom-[160px] drop-shadow-[0_7px_6px_rgba(0,0,0,0.25)]' style={{ clipPath: 'polygon(50% 0%, 100% 100%, 0% 100%)' }} />
+
+
+              <img src="images/about4.jpg" alt="" className=' w-[173px] h-[189px] rounded-[15px] relative bottom-[310px] left-[168px] drop-shadow-[0_7px_6px_rgba(0,0,0,0.25)]' />
+
+            </div>
+            <div className='w-[108vh] h-[52vh]' >
+              <h1 className='text-[3vh] font-bold'>
+                About Our Sports Management
+                System
+              </h1>
+              <br />
+              <p>SS Events Pvt. Ltd. is a Private Company established in 2014 with the goal of organizing and managing different events throughout the nations. On it's initial phase the company is coming forward with the project related to cricket. It has group of young talented cricketer both Regional and National having vision and passion with the motto of nurture and develop cricketing culture in Nepal.
+                <br /> <br />
+                Cricket being the popular and the most successful sports of the nation, the company is trying to involve the corporate giants into the game. The main theme of the project is to involve the corporate in the development of the cricketing field. Through this corporate super sixes tournament the various corporate giants directly or indirectly helping the sports for its promotion.
+                <br /> <br /> Besides organizing cricket events, SS Events Pvt. Ltd. has its Cricket Team as well which had participated in various club level tournaments as well, like Kathmandu Cricket League (KCL) 2015, Ruslan Cricket Cup 2016, YFN Shahid Smiriti Cup 2072 in which SS Cricket Team won the YFN Shahid Smiriti Cup 2072.</p>
+            </div>
+          </div>
+        </div>
+
       </div>
     </>
   )
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,12 +1,56 @@
 import React from 'react'
+import HomeServiceCard from './HomeServiceCard'
+import { TbTournament } from "react-icons/tb";
+import { MdOutlineDisplaySettings, MdOutlineMail } from "react-icons/md";
+import About from '../About Us/About';
 
 function Home() {
+  const serviceData = [{ icon: <TbTournament size={40} className='text-orange-600' />, title: 'Organize Tournaments', description: 'User can organize their tournament  and we schedule the matches automatically with fair.', }, { icon: <MdOutlineDisplaySettings size={40} className='text-orange-600' />, title: 'Display Match Results', description: 'Show match results only after matches conclude, highlighting the winning team.', }, { icon: <MdOutlineMail size={40} className='text-orange-600' />, title: 'Email Notifications', description: 'Send timely email updates to registered players about their upcoming matches and other important information', },]
+
   return (
     <>
       <div className='h-screen'>
-        Home
+        <img src="images/file.jpg" alt="" className='w-[191vh] h-[86vh] my-[7px] mx-[9vh] rounded-[33px]' />
+        <div className="absolute top-[63px] w-[191vh] h-[86vh] my-[7px] mx-[9vh] rounded-[33px] bg-[rgb(73,73,73)] opacity-[40%]"></div>
+        <div className='absolute top-[36vh] left-[34vh] w-[29%] '>
+          <h1 className='text-[5vh] text-white leading-[1] font-bold'><span className='text-orange-600'>Organize</span> or Participate in <span className='text-orange-600'>Tournament</span></h1>
+          <br />
+          <h3 className='text-[3vh] text-white leading-[1] font-bold'>Welcome to Our Sports System</h3>
+          <br />
+          <p className='text-[18px] text-white leading-[26px] font-normal'>The System where you can organize your tournament or register
+            your team to participate in tournament. You can view the match
+            results easily</p>
+        </div>
+        <div className='absolute top-[23vh] right-[20vh] space-y-7 '>
+          <img src="images/home2.jpg" alt="" className='rounded-[15px]' />
+          <img src="images/home4.jpg" alt="" className='rounded-[15px]' />
+        </div>
+        <div className='absolute top-[32vh] right-[42vh] space-y-7 '>
+          <img src="images/home1.jpg" alt="" className='rounded-[15px]' />
+          <img src="images/home3.jpg" alt="" className='rounded-[15px]' />
+        </div>
+        <h1 className='text-3xl font-bold text-center mt-8'>Why <span className='text-orange-600'> Choose Us</span></h1>
+
       </div>
+      <div className='flex justify-center space-x-24 mt-4' >
+        {serviceData.map((service, index) => (
+          <HomeServiceCard
+            key={index}
+            icon={service.icon}
+            title={service.title}
+            description={service.description}
+          />
+        ))}
+
+      </div>
+      <div className="w-full h-[4px] bg-orange-600 m-[18vh_0_0_0]"></div>
+
+      <About />
+
+
     </>
+
+
   )
 }
 
