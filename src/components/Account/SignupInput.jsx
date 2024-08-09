520629734bd1abdcfcd68d89e14b44098d4721ae
Aaditya Chaudhary <caaditya495@gmail.com>
2024-08-09 16:26:04 +0545

    modify signup

---
+++ src/components/Account/SignupInput.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+function SignupInput({type, placeholder, label, name}) {
+  return (
+      <div className='flex flex-col'>
+        <label htmlFor={name}> {label}</label>
+        <input type={type} placeholder={placeholder} id={name} className='border border-black rounded-lg py-2 pl-3 w-full font-normal'/>
+      </div>
+  )
+}
+
+export default SignupInput
