4a5f8f1a7c97c072ba2d3514bbdf571220a94402
Aaditya Chaudhary <caaditya495@gmail.com>
2024-08-16 20:50:49 +0545

    remove mt-4

--- src/components/Account/Input.jsx
+++ src/components/Account/Input.jsx
@@ -5,7 +5,7 @@ function Input({ type, placeholder, label, name, required, value}) {
   return (
     <div className='flex flex-col'>
       <label htmlFor={name}> {label} {required && <span className="text-red-500 text-md">*</span>}</label>
-      <input type={type} placeholder={placeholder} id={name} value={value} className='border rounded-md p-2 mt-4 focus:outline-orange-600' />
+      <input type={type} placeholder={placeholder} id={name} value={value} className='border rounded-md p-2 focus:outline-orange-600' />
     </div>
   )
 }
