93a56665be4c459d594c094cd7b5d025119f07fd
codewithsubash <155702363+codewithsubash@users.noreply.github.com>
2024-08-23 19:19:59 +0545

    Logo add at navbar,Inputs border color changes ProfileDropdown color change and manage some position,Navbar height and text changes, Typewriter packages and some changes in home pages

--- src/components/Account/Input.jsx
+++ src/components/Account/Input.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
 
-function Input({ type, placeholder, label, name, required, value}) {
+function Input({ type, placeholder, label, name, required, value }) {
   return (
     <div className='flex flex-col'>
       <label htmlFor={name}> {label} {required && <span className="text-red-500 text-md">*</span>}</label>
-      <input type={type} placeholder={placeholder} id={name} value={value} className='border rounded-md p-2 focus:outline-orange-600' />
+      <input type={type} placeholder={placeholder} id={name} value={value} className='border rounded-md p-2 focus:outline-orange-400' />
     </div>
   )
 }
--- src/components/Account/ProfileDropdown.jsx
+++ src/components/Account/ProfileDropdown.jsx
@@ -2,28 +2,29 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 import { logout } from '../../store/UserSlice';
 import { useDispatch } from 'react-redux';
-import { CiLogout, CiSettings } from "react-icons/ci";
+import { TbSettings } from "react-icons/tb";
+import { RiLogoutBoxLine } from "react-icons/ri";
 import { CgProfile } from "react-icons/cg";
 
 function ProfileDropdown() {
       const dispatch = useDispatch();
       return (
-            <div className='absolute bg-yellow-600 w-full right-0 py-2 px-4 rounded-b-md mt-[18vh]'>
-                  <ul className="flex flex-col gap-2 text-center">
-                        <li className="flex items-center gap-2">
-                              <CgProfile />
+            <div className='absolute flex justify-start pl-6 bg-[rgb(255,156,72,1)] w-full right-0 py-2 px-4 rounded-b-md mt-[18vh]'>
+                  <ul className="flex flex-col  gap-2 ">
+                        <li className="flex items-center gap-4">
+                              <CgProfile className='text-white ' size={21} />
                               <NavLink to="/profile">
                                     Profile
                               </NavLink>
                         </li>
-                        <li className="flex items-center gap-2">
-                              <CiSettings />
+                        <li className="flex items-center gap-4">
+                              <TbSettings className='text-white' size={21} />
                               <NavLink to="/settings">
                                     Settings
                               </NavLink>
                         </li>
-                        <li className="flex items-center gap-2">
-                              <CiLogout />
+                        <li className="flex items-center gap-4">
+                              <RiLogoutBoxLine className='text-white' size={21} />
                               <NavLink to="/" onClick={() => dispatch(logout())}>
                                     Logout
                               </NavLink>
