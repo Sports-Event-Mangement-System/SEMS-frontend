fb6272c42775f4b3225cc37c881db77e02813838
codewithsubash <155702363+codewithsubash@users.noreply.github.com>
2024-09-13 08:26:55 +0545

    Correcting Certain mistake in previous contact push of contact admin detail

--- src/components/Admin/Items/AdminContact.jsx
+++ src/components/Admin/Items/AdminContact.jsx
@@ -117,8 +117,8 @@ export default function AdminContact() {
                 <tr className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white text-start" key={contact.id}>
                   <td className="px-6 py-3">{index + 1}</td>
                   <td className="px-6 py-3">{contact.first_name + ' ' + contact.last_name}</td>
-                  <td className="px-6 py-3">{contact.phone_number}</td>
                   <td className="px-6 py-3">{contact.email}</td>
+                  <td className="px-6 py-3">{contact.phone_number}</td>
                   <td className="px-6 py-3">
                     {contact.message.split(' ').slice(0, 3).join(' ') + '...'}
                   </td>
