5b558ad6e90ba74363bfa96e5d3150596fda2b7d
codewithsubash <155702363+codewithsubash@users.noreply.github.com>
2024-11-18 22:27:24 +0545

    dashboard card refinement

--- src/components/Ui/Card/Card.jsx
+++ src/components/Ui/Card/Card.jsx
@@ -1,9 +1,10 @@
-
 import React from 'react';
 
-export default function Card({ children }) {
+export default function Card({ children, background }) {
     return (
-        <div className="bg-white w-fit h-fit rounded-md border border-[#c6c6c6] cursor-pointer transition-transform transform hover:-translate-y-2 hover:shadow-lg">
+        <div
+            className={`w-fit h-fit rounded-md border border-[#c6c6c6] cursor-pointer transition-transform transform hover:-translate-y-2 hover:shadow-lg ${background}`}
+        >
             {children}
         </div>
     );
